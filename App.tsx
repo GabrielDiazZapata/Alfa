77c194d9f5f259de5c2037f5022bc85f5d14f4ed
GabrielDiazZapata <usr3566@salesianos-lacuesta.net>
2023-12-04 20:21:05 +0000

    feat:(screen-welcome):login screen created

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Welcome from './screens/Welcome';
+import Login from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: "LOGIN",
+            headerTintColor: "white",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#525FE1" },
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Welcome}
+          options={{
+            title: "HOME",
+            headerTintColor: "white",
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#525FE1" },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,91 @@
+import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity, ImageBackground } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native';
+
+type LoginProps = {
+  navigation: any; // Asegúrate de usar el tipo de navegación adecuado aquí
+};
+export default function Login(props: LoginProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigation = useNavigation();
+
+    function logearte(): void {
+        throw new Error('Function not implemented.');
+    }
+
+  return (
+    <View style={styles.padre}>
+      <View>
+        <Image source={require('../assets/LOGO_GABRIEL.png')} style={styles.profile} />
+      </View>
+
+      <View style={styles.tarjeta}>
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder=' Correo electrónico ' style={{ paddingHorizontal: 15 }} onChangeText={(text) => setEmail(text)} />
+        </View>
+
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder='Contraseña' style={{ paddingHorizontal: 15 }} onChangeText={(text) => setPassword(text)} secureTextEntry={true} />
+        </View>
+
+        <View style={styles.PadreBoton}>
+          <TouchableOpacity style={styles.cajaBoton} onPress={logearte}>
+            <Text style={styles.textoBoton}>Iniciar Sesión</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  padre: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  profile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderBlockColor: 'white'
+  },
+  tarjeta: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: '90%',
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  cajaTexto: {
+    paddingVertical: 20,
+    backgroundColor: '#cccccc40',
+    borderRadius: 30,
+    marginVertical: 10
+  },
+  PadreBoton: {
+    alignItems: 'center',
+  },
+  cajaBoton: {
+    backgroundColor: '#525FE1',
+    borderRadius: 30,
+    paddingVertical: 20,
+    width: 150,
+    marginTop: 20
+  },
+  textoBoton: {
+    textAlign: 'center',
+    color: 'white'
+  }
+});
